d2c46a7fe26db1a29b8cd26f7585ab4dd2242ecd
Alexander Olferuk <a.olferuk@sportradar.com>
2025-07-31 01:12:19 +0300

    update .gitignore and add new files

---
+++ src/intro_splash.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var alpha_change_speed := 2.0
+
+var alpha_change_sign := -1.0
+var alpha_state := 1.0
+
+@onready var label = $ClickToContinueLabel
+
+func _process(delta):
+	modulate_alpha(delta)
+
+func _unhandled_input(event):
+	if event.is_pressed():
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func modulate_alpha(delta):
+	var da: float = delta * alpha_change_sign * alpha_change_speed
+	alpha_state += da
+	
+	if alpha_state < 0:
+		alpha_change_sign = 1.0
+	elif alpha_state > 1:
+		alpha_change_sign = -1.0
+	alpha_state = clampf(alpha_state, 0, 1)
+	
+	label.modulate = Color(1, 1, 1, alpha_state)
