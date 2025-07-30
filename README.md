55c189c102f365b3170d6067ff2d6b951d3b6a21
Alexander Olferuk <a.olferuk@sportradar.com>
2025-07-31 02:09:50 +0300

    Added font

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Participants: @olferuk, @lowmonopolium
 
 ## Ant Mill
 
+### TODO:
+
+* Add credits for font usage due to [CC 3.0](https://fontstruct.com/fontstructions/download/1178884).
--- src/main_menu.gd
+++ src/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_new_game_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/intro_splash.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/intro_splash.tscn")
 
 func _on_exit_button_pressed():
 	get_tree().quit()
