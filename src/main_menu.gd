8f472ae9f0cb68be44c8e7df4aa777fcd78e2c6b
Aleksandr Sidorov <lowmonopolium@gmail.com>
2025-07-31 17:47:20 +0300

    add walk and idle animations for player. Move sprite towards 8 axes

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_new_game_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/menus/intro_splash.tscn")
+	get_tree().change_scene_to_file("res://scenes/game_objects/player.tscn")
 
 func _on_exit_button_pressed():
 	get_tree().quit()
