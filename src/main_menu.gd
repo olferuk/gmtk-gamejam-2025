d6f61760ead83db6f4950450d0b9a3845ea122da
Alexander Olferuk <a.olferuk@sportradar.com>
2025-08-03 17:06:49 +0300

    Starting the game from a certain level

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -1,7 +1,8 @@
 extends Control
 
 func _on_new_game_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/ants_test.tscn")
+	LevelManager.load_level_1()
+	get_tree().change_scene_to_file("res://scenes/ui/start_screen.tscn")
 
 func _on_exit_button_pressed():
 	get_tree().quit()
